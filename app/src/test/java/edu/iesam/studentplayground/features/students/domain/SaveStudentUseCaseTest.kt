03a842a252eae376111824dea261bee6abf2bb68
Dani <daniel.lopgon.4@educa.jcyl.es>
2025-09-26 17:01:54 +0200

    commit 5: fix github problems

---
+++ app/src/test/java/edu/iesam/studentplayground/features/students/domain/SaveStudentUseCaseTest.kt
@@ -0,0 +1,28 @@
+package edu.iesam.studentplayground.features.students.domain
+
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Assert.*
+import org.junit.Test
+
+class SaveStudentUseCaseTest {
+
+    /**
+     * When .... Then ....
+     */
+    @Test
+    fun `when invoke then save student`(){
+        //Given
+        val studentRepositoryMock = mockk<StudentRepository>(relaxed = true)
+        val saveStudentUseCase = SaveStudentUseCase(studentRepositoryMock)
+        val student = Student("001","Name1")
+
+        //When
+        saveStudentUseCase.save(student)
+
+        //Then
+        verify(exactly = 1) { studentRepositoryMock.save(student) }
+
+    }
+
+}
