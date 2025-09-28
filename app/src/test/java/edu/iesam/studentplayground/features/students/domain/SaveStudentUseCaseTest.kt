c331f0648fd66b4a3577dbcb1aa0fd7381fa4d19
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2025-09-28 13:49:30 +0200

    commit: I fixed errors of dependencies

--- app/src/test/java/edu/iesam/studentplayground/features/students/domain/SaveStudentUseCaseTest.kt
+++ app/src/test/java/edu/iesam/studentplayground/features/students/domain/SaveStudentUseCaseTest.kt
@@ -1,28 +1,49 @@
 package edu.iesam.studentplayground.features.students.domain
 
-import io.mockk.mockk
-import io.mockk.verify
 import org.junit.Assert.*
 import org.junit.Test
+import io.mockk.mockk
+import io.mockk.verify
 
 class SaveStudentUseCaseTest {
 
-    /**
-     * When .... Then ....
-     */
     @Test
     fun `when invoke then save student`(){
         //Given
         val studentRepositoryMock = mockk<StudentRepository>(relaxed = true)
         val saveStudentUseCase = SaveStudentUseCase(studentRepositoryMock)
-        val student = Student("001","Name1")
+        val student = Student("001","Name")
 
         //When
         saveStudentUseCase.save(student)
 
         //Then
-        verify(exactly = 1) { studentRepositoryMock.save(student) }
-
+        verify (exactly = 1){ studentRepositoryMock.save(student)  }
     }
 
+    @Test
+    fun `when student has empty exp`() {
+        //Given
+        val studentRepositoryMock = mockk<StudentRepository>(relaxed = true)
+        val saveStudentUseCase = SaveStudentUseCase(studentRepositoryMock)
+        val student = Student("","Name")
+
+        //When && Then
+        assertThrows(IllegalArgumentException::class.java){
+            saveStudentUseCase.save(student)
+        }
+    }
+
+    @Test
+    fun `when student has empty name`() {
+        //Given
+        val studentRepositoryMock = mockk<StudentRepository>(relaxed = true)
+        val saveStudentUseCase = SaveStudentUseCase(studentRepositoryMock)
+        val student = Student("003","")
+
+        //When && Then
+        assertThrows(IllegalArgumentException::class.java){
+            saveStudentUseCase.save(student)
+        }
+    }
 }
