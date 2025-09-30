a1e46190a910f837ac1e22cf814e9fccec624455
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2025-09-30 07:30:31 +0200

    commit: I deleted comments

--- app/src/main/java/edu/iesam/studentplayground/features/students/domain/errors/ErrorApp.kt
+++ app/src/main/java/edu/iesam/studentplayground/features/students/domain/errors/ErrorApp.kt
@@ -4,5 +4,4 @@ sealed class ErrorApp : Throwable() {
     object EmptyExpedient : ErrorApp()
     object EmptyName : ErrorApp()
     object StudentAlreadyExists : ErrorApp()
-    //luego lo borro
 }
